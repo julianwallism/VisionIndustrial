49a2b41239bc1de4dd1da6c51a26d82fc4543b9a
Julián Wallis <wallismedina@gmail.com>
2022-02-25 18:00:26 +0100

    update

--- Lab01/Lab01.m
+++ Lab01/Lab01.m
@@ -1,21 +1,25 @@
+%%%%%%%%%%%%%%%%%
+%%    LAB 01   %%
+%%%%%%%%%%%%%%%%%
+
 %{
     Matrices and Matlab
 %}
 
 % 1. Create a 5×5 matrix of ones (A).
-A = ones(5,5);
+A = ones(5);
 
 % 2. Create a 5×3 matrix of ones (B).
 B = ones(5,3);
 
 % 3. Create a 3×3 matrix of zeros (C).
-C = zeros(3,3);
+C = zeros(3);
 
 % 4. Create a 4×4 matrix with equal row, column, and diagonal sums (D).
 D = magic(4);
 
 % 5. Create a 4×4 random matrix whose values are uniformly distributed between 0 and 1 (E).
-E = rand(4,4);
+E = rand(4);
 
 % 6. Create a 5×5 identity matrix (F).
 F = eye(5); % Multiplicar una matriz por su inversa da como resultado la matriz identidad -> F = A*inv(A)
@@ -60,10 +64,13 @@ indF = find(F)
 F(indF)=-1 
 
 % 20. Given those indices, transform them to subscripts (row and column).
-ind2sub(size(F),indF)
+[rowsSub, colsSub] = ind2sub(size(F),indF)
 
 % 21. Use those indices to set to -2 all positions in the matrix that are just above a value equal to -1 (except in row 1).
-F(indF>=-1)=-2
+colsSub=colsSub(2:end)
+rowsSub=rowsSub(2:end)-1
+indF = sub2ind(size(F),rowsSub, colsSub)
+F(indF)=-2
 
 % 22. Given a 10×10 matrix, set the values of the even columns to zero.
 G = ones(10);
@@ -97,34 +104,57 @@ I(2:tam-1,2:tam-1)=1
 
 % 1. Open and display the image landscape.jpg. Determine the dimensions of the image. Is this a color image?
 Img = imread("landscape.jpg");
-imshow(Img)
+%figure, imshow(Img)
 imfinfo("landscape.jpg")
 % The size of the image is 640*480, and its colortype is truecolor
 
 % 2. Convert the image to grayscale, and display the result. 
 % Then, save this resulting image into another file called landscapegray.jpg. What is the data type of the pixels?
 grayImg = rgb2gray(Img);
-imshow(grayImg)
-Image.save(landscapegray.jpg)
+%figure, imshow(grayImg)
+imwrite(grayImg,"landscapegray.jpg")
 
 % 3. Rescale the pixel values to the range [0, 1] and convert the image to double precision.
 normImage = mat2gray(grayImg);
-DP = im2double(normImage)
+DP = im2double(normImage);
 
 % 4. Display the gray scale image as a three-dimensional plot. Compare the results using two functions: mesh and surf.
-surf(grayImg)
-mesh(grayImg)
+surf(grayImg);
+mesh(grayImg);
 
 % 5. Convert the gray scale image to an indexed image with a colormap of 16 components and display the result. 
 % Do you observe differences between the original and the indexed images?
-[X, map]] = gray2ind(grayImg, 16)
+[X, map] = gray2ind(grayImg, 16);
 
 % 6. Write a Matlab script for generating the negative of the image moon.bmp. 
 % First of all do it using nested loops, and then using the matlabs ability to perform vectorized operations. The final results should look like this:
 
+moon = imread("moon.bmp");
+negMoon = uint8(zeros(size(moon)));
+[rows, cols] = size(moon);
+for i = 1:rows;
+    for j = 1:cols;
+        negMoon(i,j)=255-moon(i,j);
+    end
+end
+%figure, imshow(negMoon)
+
+negMoon2 = uint8(zeros(size(moon)));
+for i = 1:size(moon);
+        negMoon(i)=255-moon(i);
+end
+%figure, imshow(negMoon)
 
 % 7. Write a Matlab script which flips the image moon.bmp vertically. 
-% Don t use nested loops. Use only subscripting. Have a look to functions like flipud or fliplr, but don’t use them this time. The final result should look like this:
+% Dont use nested loops. Use only sub-scripting. Have a look to functions like flipud or fliplr,
+% but don-t use them this time.
+
+moon = imread("moon.bmp");
+flippedMoon = uint8(zeros(size(moon)));
+flippedMoon(1:end,1:end) = moon(end:-1:1,1:end);
+%figure, imshow(flippedMoon)
+
+
 
 
 
