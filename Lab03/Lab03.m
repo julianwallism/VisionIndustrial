016ed39737ef24583ab1fb9b4c822a6ba7a1d57e
Julián Wallis <wallismedina@gmail.com>
2022-03-10 11:31:50 +0100

    Lab03 - Ejercicio 5
    Falta mirar porque se normaliza raro

--- Lab03/Lab03.m
+++ Lab03/Lab03.m
@@ -39,9 +39,9 @@ subplot(3,2,6), imhist(white), title('Hist white');
 % with their corresponding histograms. Compare the histograms of the 
 % adjusted images with the original ones.
 
-low_norm = double(low)/255
-black_norm = double(black)/255;
-white_norm = double(white)/255;
+low_norm = rescale(low);
+black_norm =rescale(black);
+white_norm = rescale(white)
 
 low_adjust = adjust(low_norm);
 black_adjust = adjust(black_norm);
@@ -56,16 +56,26 @@ subplot(3,2,4), imhist(black_adjust), title('hist black adjust');
 
 subplot(3,2,5), imshow(white_adjust), title('white adjust');
 subplot(3,2,6), imhist(white_adjust), title('hist white adjust');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 4. Apply the function provided by Matlab for adjusting the contrast of 
-% the three images, and display the resulting images with their histograms
+% the three images, and display the resulting images with their histograms
 % Are these histograms the same as the ones obtained in the previous 
 % exercise? Why?
 
+low_adjust_m = imadjust(low);
+black_adjust_m = imadjust(black);
+white_adjust_m = imadjust(white);
+
 figure('Name', 'Matlab Adjust');
-subplot(3,1,1), imadjust(low), title('low adjust');
-subplot(3,1,2), imadjust(black), title('black adjust');
-subplot(3,1,3), imadjust(white), title('white adjust');
+subplot(3,2,1), imshow(low_adjust_m), title('low adjust');
+subplot(3,2,2), imhist(low_adjust_m), title('hist low adjust');
+
+subplot(3,2,3), imshow(black_adjust_m), title('black adjust');
+subplot(3,2,4), imhist(black_adjust_m), title('hist black adjust');
+
+subplot(3,2,5), imshow(white_adjust_m), title('white adjust');
+subplot(3,2,6), imhist(white_adjust_m), title('hist white adjust');
 
 
 
@@ -74,12 +84,25 @@ subplot(3,1,3), imadjust(white), title('white adjust');
 % to obtain the complement (negative) of an image?. Think about it, try to 
 % perform this operation using the three images and show the results.
 
+low_adjust_neg = imadjust(low,[0,1],[1,0]);
+black_adjust_neg = imadjust(black,[0,1],[1,0]);
+white_adjust_neg = imadjust(white,[0,1],[1,0]);
 
+figure('Name', 'Matlab Adjust Negative');
+subplot(3,2,1), imshow(low_adjust_neg), title('low adjust');
+subplot(3,2,2), imhist(low_adjust_neg), title('hist low adjust');
+
+subplot(3,2,3), imshow(black_adjust_neg), title('black adjust');
+subplot(3,2,4), imhist(black_adjust_neg), title('hist black adjust');
+
+subplot(3,2,5), imshow(white_adjust_neg), title('white adjust');
+subplot(3,2,6), imhist(white_adjust_neg), title('hist white adjust');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 6. Given the three versions of the images: the original one, the adjusted
 % one obtained with your function and the adjusted one using the Matlabs 
 % function, which one would you use for a further processing step? Why?
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -145,7 +168,7 @@ function imadj = adjust(image)
    O_max=1;
    for i = 1:length(image);
        for j = 1:length(image);
-        imadj(i,j)=(O_max-O_min)*((image(i,j)-I_min)/(I_max-I_min))+O_min;
+           imadj(i,j)=(O_max-O_min)*((image(i,j)-I_min)/(I_max-I_min))+O_min;
        end
    end
 end
